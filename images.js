036982760b1f7b299031d93ed663ec01ee00e98a
Lucas Rocha <designer.br.vG@gmail.com>
2026-07-22 11:13:04 -0400

    Create images.js

---
+++ images.js
@@ -0,0 +1,28 @@
+// ==========================================================================
+// ARQUIVO DE DADOS: IMAGENS EM BASE64
+// Cole suas strings Base64 dentro das aspas correspondentes abaixo.
+// Não apague as aspas nem as vírgulas!
+// ==========================================================================
+
+const base64Images = {
+    // ---------------------------------------------------
+    // LOGOMARCA
+    // ---------------------------------------------------
+    logo: "", // Cole o Base64 da Logomarca aqui
+
+    // ---------------------------------------------------
+    // PRODUTOS
+    // ---------------------------------------------------
+    santinho: "",       
+    cartao: "",         
+    colinha: "",        
+    jornal: "",         
+    perfurado: "",      
+    adesivo: "",        
+    pragao: "",         
+    praguinha: "",      
+    bandeira: "",       
+    windbanner: "",     
+    balcao: "",         
+    ventarola: ""       
+};
